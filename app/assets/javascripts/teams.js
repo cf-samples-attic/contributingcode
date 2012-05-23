066ff2303a97469d348bc8171566359c630f49a0
Magi <mahilis@live.com>
2012-05-23 16:29:05 -0700

    scrollg on button click added

--- app/assets/javascripts/teams.js
+++ app/assets/javascripts/teams.js
@@ -1,5 +1,40 @@
 
 $(document).ready(function(){
+
+    function scrollToElement(selector, callback){
+    var animation = {scrollTop: $(selector).offset().top};
+    $('html,body').animate(animation, 'slow', 'swing', function() {
+        if (typeof callback == 'function') {
+            callback();
+        }
+        callback = null;
+    });
+    }
+
+    
+    $(".b1").click(function(){
+        var button_value = this.value;
+        console.log(button_value);
+        if(this.value=="Home")
+            scrollToElement('.home');
+        else if(this.value=="Bootcamp") 
+            scrollToElement('.bootcamp');
+        else if(this.value=="Rules") 
+            scrollToElement('.rules');
+        else if(this.value=="Schedule") 
+            scrollToElement('.schedule');
+        else if(this.value=="Venue") 
+            scrollToElement('.venue');
+        else if(this.value=="Prizes") 
+            scrollToElement('.prizes');
+        else if(this.value=="Teams") 
+            scrollToElement('.teams');
+        else 
+            console.log("invalid");
+    });
+
+
+
     $("#team_form").submit(function(){
         event.preventDefault();
         var o = {}
