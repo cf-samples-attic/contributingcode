78c84aa88eeb8a3bfc03d78179be3ea59aa01cff
Magi <mahilis@live.com>
2012-05-23 18:34:42 -0700

    moved pages to partials

--- config/environment.rb
+++ config/environment.rb
@@ -5,12 +5,22 @@ require File.expand_path('../application', __FILE__)
 Tumblrtv::Application.initialize!
 
 
+# ActionMailer::Base.smtp_settings = {
+#   :user_name            => "appcloud",
+#   :password             => "@ppcl0ud",
+#   :domain               => "cloudfoundry.org",
+#   :address              => "smtp.las01.emcatmos.com",
+#   :port                 => 587,
+#   :authentication       => :plain,
+#   :enable_starttls_auto => false
+# }
+
 ActionMailer::Base.smtp_settings = {
-  :user_name            => "appcloud",
-  :password             => "@ppcl0ud",
-  :domain               => "cloudfoundry.org",
-  :address              => "smtp.las01.emcatmos.com",
+  :user_name            => "cours.ly",
+  :password             => "mystartup",
+  :domain               => "cours.ly",
+  :address              => "smtp.sendgrid.net",
   :port                 => 587,
   :authentication       => :plain,
-  :enable_starttls_auto => false
+  :enable_starttls_auto => true
 }
