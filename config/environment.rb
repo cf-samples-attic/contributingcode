2d2fc0fcf6acacaa64929ff72a57c6d2018c443d
Magi <mahilis@live.com>
2012-05-25 10:13:10 -0700

    added bootstrap

--- config/environment.rb
+++ config/environment.rb
@@ -4,10 +4,21 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Tumblrtv::Application.initialize!
 
+
+# ActionMailer::Base.smtp_settings = {
+#   :user_name            => "appcloud",
+#   :password             => "@ppcl0ud",
+#   :domain               => "cloudfoundry.org",
+#   :address              => "smtp.las01.emcatmos.com",
+#   :port                 => 587,
+#   :authentication       => :plain,
+#   :enable_starttls_auto => false
+# }
+
 ActionMailer::Base.smtp_settings = {
-  :user_name            => "mvmware.com776",
-  :password             => "sharedpass",
-  :domain               => "cloudfoundry.org",
+  :user_name            => "cours.ly",
+  :password             => "mystartup",
+  :domain               => "cours.ly",
   :address              => "smtp.sendgrid.net",
   :port                 => 587,
   :authentication       => :plain,
