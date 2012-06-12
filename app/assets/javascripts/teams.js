38ac09a31caa1a242a36f0dd2709c99026c521de
Magi <mahilis@live.com>
2012-06-12 15:20:01 -0700

    custom shares

--- app/assets/javascripts/teams.js
+++ app/assets/javascripts/teams.js
@@ -15,6 +15,45 @@ $(document).ready(function(){
       'X-CSRF-Token': jQuery("meta[name='csrf-token']").attr('content')
     }
   });
+
+  // Share
+  $("#ShareTweet").click(function(e) {
+      e.preventDefault()
+      var self = this
+      var turl = "https://twitter.com/intent/tweet?text=" + "contributingcode.cloudfoundry.com&related=cloudfoundry" 
+      var width  = 575,
+        height = 400,
+        left   = ($(window).width()  - width)  / 2,
+        top    = ($(window).height() - height) / 2,
+        url    = this.href,
+        opts   = 'status=1' +
+                 ',width='  + width  +
+                 ',height=' + height +
+                 ',top='    + top    +
+                 ',left='   + left
+
+      window.open(turl, 'twitterpop', opts)
+      return false;
+    })
+
+    $("#ShareFacebook").click(function(e) {
+      e.preventDefault()
+      var self = this
+      var fburl = "http://www.facebook.com/sharer.php?u=" + "contributingcode.cloudfoundry.com"
+      var width  = 575,
+        height = 400,
+        left   = ($(window).width()  - width)  / 2,
+        top    = ($(window).height() - height) / 2,
+        url    = this.href,
+        opts   = 'status=1' +
+                 ',width='  + width  +
+                 ',height=' + height +
+                 ',top='    + top    +
+                 ',left='   + left  
+
+      window.open(fburl, 'facebookpop', opts)
+      return false;
+    })
     
   // My team js hide and show  
   $(".team_btn").click(function(){
