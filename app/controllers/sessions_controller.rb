823175a101e20156c7732b8b06e1f21a660c4c6a
ciberch <monica.keller@gmail.com>
2012-06-14 18:00:15 -0700

    Removed current user requirement

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,31 +1,32 @@
 class SessionsController < ApplicationController
 
-  # callback form github 
+  # callback form github
   # Create user for first time
-  # Redirect to registration form unless email is filled 
-  # Session is not created unless user has email 
+  # Redirect to registration form unless email is filled
+  # Session is not created unless user has email
   def create
     auth = request.env["omniauth.auth"]
-    @user = User.find_by_uid(auth["uid"]) 
+    @user = User.find_by_uid(auth["uid"])
     if @user
       session[:user_id] = @user.id
-      redirect_to root_url, :notice => "Signed in!" 
-    else 
-      session[:auth] = auth  
+      redirect_to root_url, :notice => "Signed in!"
+    else
+      session[:auth] = auth
+      @teams = []
       @name = auth[:info][:name]
       @email = auth[:info][:email]
       @avatar = auth[:extra][:raw_info][:avatar_url]
       @handle = auth["extra"]["raw_info"]["login"]
       render "home/index"
       return
-    end 
+    end
   end
 
   def failure
     render :text => "Failed!"
   end
 
-  # logout 
+  # logout
   def destroy
     session[:user_id] = nil
     redirect_to root_url, :notice => "Signed out!"
