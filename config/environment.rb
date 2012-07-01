ae737fb986aea876ba40dd9d3643823125490cc3
mahil <mahilis@live.com>
2012-07-01 09:14:15 -0700

    added sendgrid env vars

--- config/environment.rb
+++ config/environment.rb
@@ -4,9 +4,9 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Tumblrtv::Application.initialize!
 
-ActionMailer::Base.smtp_settings = {
-  :user_name            => "mvmware.com814",
-  :password             => "sharedpass",
+ActionMailer::Base.smtp_settings = {	
+  :user_name            => ENV['sendgrid_username'],
+  :password             => ENV['sendgrid_password'],
   :domain               => "cloudfoundry.org",
   :address              => "smtp.sendgrid.net",
   :port                 => 587,
